3acedcec62754b65c36d94d698215c96b05f601e
Malte Rohde <malte.rohde@flavoursys.com>
2013-11-05 15:21:22 -0500

    Status command: Move functionality to Repository.

--- lib/mgit/repository.rb
+++ lib/mgit/repository.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 module MGit
   class Repository
     attr_reader :name, :path
@@ -5,6 +7,50 @@ module MGit
     def initialize(name, path)
       @name = name
       @path = path
+    end
+
+    def flags
+      flags = Set.new
+      status_lines do |s|
+        case s.split[0]
+        when 'A'
+          flags << :index
+        when 'M'
+          flags << :dirty
+        when '??'
+          flags << :untracked
+        when '##'
+          flags << :diverged
+        end
+      end
+      flags
+    end
+
+    def divergence
+      divergence = []
+      status_lines do |s|
+        if s.split[0] == '##'
+          if(m = /## ([\w,\/]+)\.\.\.([\w,\/]+) \[(\w+) (\d+)\]/.match(s))
+            if m[3] =~ /behind/
+              divergence << { :behind => { :branch => m[2], :by => m[4] } }
+            else
+              divergence << { :ahead => { :branch => m[2], :by => m[4] } }
+            end
+          end
+        end
+      end
+      divergence
+    end
+
+  private
+  
+    def status_lines
+      Dir.chdir(path) do
+        status = `git status --short --branch --ignore-submodules`.split("\n")
+        status.each do |s|
+          yield s
+        end
+      end
     end
   end
 end
