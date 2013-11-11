13098c26bed756c9cb1bb6785b75013c781c0e20
Malte Rohde <malte.rohde@flavoursys.com>
2013-11-10 23:48:30 -0500

    Add: Refuse to overwrite named repository path.

--- lib/mgit/commands/add.rb
+++ lib/mgit/commands/add.rb
@@ -5,7 +5,8 @@ module MGit
       raise CommandUsageError.new('First argument must be a path to a git repository.', self) unless is_git_dir?(path)
 
       name = (args.size == 2) ? args[1] : (ask('Name of the repository? ') { |q| q.default = File.basename(path) })
-
+      raise CommandUsageError.new("Repository named #{name} already exists with different path.", self) unless is_new_or_same?(name, path)
+      
       Registry.add(name, path)
     end
 
@@ -24,6 +25,11 @@ module MGit
     register_command :add
 
   private
+
+    def is_new_or_same?(name, path)
+      repo = Registry.find { |r| r.name == name }
+      repo.nil? || repo.path == path
+    end
 
     def is_git_dir?(path)
       File.directory?(path) && (File.directory?(File.join(path, '.git')) || File.file?(File.join(path, 'HEAD')))
