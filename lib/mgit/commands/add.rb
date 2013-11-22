b18bcfaf31d0538f11abd73ba7c50ecaf8b24fa5
Malte Rohde <malte.rohde@flavoursys.com>
2013-11-22 10:40:29 -0500

    Add: Disallow bare repositories.

--- lib/mgit/commands/add.rb
+++ lib/mgit/commands/add.rb
@@ -2,7 +2,9 @@ module MGit
   class AddCommand < Command
     def execute(args)
       path = File.expand_path(args[0])
+      raise CommandUsageError.new('First argument must be a path to a directory.', self) unless File.directory?(path)
       raise CommandUsageError.new('First argument must be a path to a git repository.', self) unless is_git_dir?(path)
+      raise CommandUsageError.new('Sorry, mgit can not handle bare repositories.', self) if is_bare?(path)
 
       name = (args.size == 2) ? args[1] : (ask('Name of the repository? ') { |q| q.default = File.basename(path) })
       raise CommandUsageError.new("Repository named #{name} already exists with different path.", self) unless is_new_or_same?(name, path)
@@ -32,7 +34,11 @@ module MGit
     end
 
     def is_git_dir?(path)
-      File.directory?(path) && (File.directory?(File.join(path, '.git')) || File.file?(File.join(path, 'HEAD')))
+      Dir.chdir(path) { `git status 2>&1` !~ /fatal: Not a git repository/ }
+    end
+
+    def is_bare?(path)
+      Dir.chdir(path) { `git status 2>&1` =~ /fatal: This operation must be run in a work tree/}
     end
   end
 end
