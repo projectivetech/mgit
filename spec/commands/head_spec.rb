7e8787f49fd785b9dd14b008907d02141fb8ecc9
Malte Rohde <malte.rohde@flavoursys.com>
2013-12-06 13:32:09 +0100

    Add specs for head.

---
+++ spec/commands/head_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'status command' do
+  include_context 'silence'
+  include_context 'managed_repository', :history
+
+  subject(:command) do
+    MGit::Command.create('head')
+  end
+
+  it 'reports the current branch' do
+    puts MGit::Registry.all.inspect
+    command.execute([])
+    expect(@stdout.string).to match(/master/)
+  end
+
+  it 'reports the current HEAD commit' do
+    command.execute([])
+    Dir.chdir(@repo_path) do
+      commit = `git rev-parse --verify --short HEAD`
+      expect(@stdout.string).to include(commit)
+    end
+  end
+end
